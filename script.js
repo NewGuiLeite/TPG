60b8b3326be14781fe283b3a00e96bf0e7aaae88
Guilherme Leite <128203474+NewGuiLeite@users.noreply.github.com>
2023-12-05 18:07:38 -0300

    novo

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const convertButton = document.getElementById('convertButton');
+    // Função para obter e preencher o valor atual do dólar
+    function obterValorDolarAtual() {
+        const apiUrl = 'https://economia.awesomeapi.com.br/last/USD-BRL';
 
-    if (convertButton) {
-        convertButton.addEventListener('click', function () {
-            const inputJpg = document.getElementById('inputJpg');
-            const jpgFile = inputJpg.files[0];
-
-            if (!jpgFile) {
-                alert('Selecione um arquivo JPG.');
-                return;
-            }
-
-            const reader = new FileReader();
-
-            reader.onload = function (e) {
-                const imgData = e.target.result;
-
-                const pdf = new window.jspdf.jsPDF(); // Corrigir a referência para jsPDF
-                pdf.addImage(imgData, 'JPEG', 10, 10, 190, 150);
-                pdf.save('output.pdf');
-
-                document.getElementById('resultMessage').innerText = 'Conversão realizada com sucesso!';
-            };
-
-            reader.readAsDataURL(jpgFile);
-        });
-    } else {
-        console.error('Elemento convertButton não encontrado.');
+        axios.get(apiUrl)
+            .then(response => {
+                const valorDolarAtual = response.data.USDBRL.bid;
+                document.getElementById('valorDolarUsuario').value = valorDolarAtual;
+            })
+            .catch(error => {
+                console.error('Erro ao obter a taxa de câmbio:', error);
+                alert('Erro ao obter a taxa de câmbio. Tente novamente mais tarde.');
+            });
     }
+
+    // Chamar a função ao carregar a página
+    obterValorDolarAtual();
+
+    // Adicionar evento de clique ao botão de converter
+    document.getElementById('button-converter').addEventListener('click', function () {
+        const valorReal = document.getElementById('valorReal').value;
+        const valorDolarUsuario = document.getElementById('valorDolarUsuario').value;
+
+        if (!valorDolarUsuario) {
+            alert('Por favor, informe o valor do dólar.');
+            return;
+        }
+
+        const valorDolar = (valorReal / valorDolarUsuario).toFixed(2);
+        document.getElementById('valorDolar').innerText = `$${valorDolar}`;
+    });
 });
